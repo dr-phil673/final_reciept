39fc630694e8561f3d929bc940b2967a0dcefd4f
dr-phil673 <141211581+dr-phil673@users.noreply.github.com>
2023-08-09 00:04:02 +0300

    Update main.py

--- main.py
+++ main.py
@@ -1,26 +1,20 @@
-#import principal
+
 import streamlit as st
-#imports relacionados
 from PIL import Image
 import pytesseract
-#metodos internos
 import functions.functions as fc
 
 class OCR:
 
     def __init__(self):
-        #altera titulo da pagina
         st.set_page_config(page_title="Python OCR")
-        #inicializa variveis
         self.texto = ""
         self.analisar_texto = False
 
     def inicial(self):
-        #conteudo inicial da pagina
         st.title("OCR Programa")
         st.write("Optical Character Recognition (OCR) implementado com Python")
         imagem = st.file_uploader("Selecione alguma imagem", type=["png","jpg"])
-        #se selecionar alguma imagem...
         if imagem:
             img = Image.open(imagem)
             st.image(img, width=350)
@@ -28,18 +22,15 @@ class OCR:
             self.texto = self.extrair_texto(img)
             st.write("{}".format(self.texto))
             
-            #Opcao de analisar texto
             self.analisar_texto = st.sidebar.checkbox("Analisar texto")
             if self.analisar_texto==True:
                 self.mostrar_analise()
     
     def extrair_texto(self, img):
-        #O comando que extrai o texto da imagem
         texto = pytesseract.image_to_string(img, lang="por")
         return texto
     
     def mostrar_analise(self):
-        #busca CPF, datas e palavras boas e mas na extracao
         cpf = fc.buscar_cpf(self.texto)
         datas = fc.buscar_data(self.texto)
         p_boas, percentual_bom = fc.buscar_palavras_boas(self.texto)
