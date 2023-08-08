a95f1b02dd1b6422b6aa1cd0af3c937bc1e8e19d
dr-phil673 <141211581+dr-phil673@users.noreply.github.com>
2023-08-09 00:40:49 +0300

    Update functions.py

--- functions/functions.py
+++ functions/functions.py
@@ -1,34 +1,6 @@
 
 import re
 
-def buscar_cpf(texto):
-    """Funcao para buscar CPF no texto que foi extraido.
-    \nRetorna uma lista com os CPFs encontrados ou False se não existir.
-    \nPara ser encontrado, o valor deve estar no formato xxx.xxx.xxx-xx.
-    \nEx.:
-    texto = texto extraido da imagem
-    CPFs = buscar_cpf(texto)"""
-    CPF = re.findall('[0-9]{3}.[0-9]{3}.[0-9]{3}-[0-9]{2}', texto)
-    if len(CPF)>0:
-        return CPF
-    else:
-        False
-
-
-def buscar_data(texto):
-    """Funcao para buscar datas no texto que foi extraido.
-    \nRetorna uma lista com as datas encontradas ou False se não existir.
-    \nPara ser encontrado, o valor deve estar no formato xx/xx/xxxx.
-    \nEx.:
-    texto = texto extraido da imagem
-    Datas = buscar_data(texto)"""
-    DATA = re.findall('[0-9]{2}/[0-9]{2}/[0-9]{4}', texto)
-    if len(DATA)>0:
-        return DATA
-    else:
-        False
-
-
 
 def sumarizar_cpf(cpf):
     """Funcao para deixar o CPF em string.
