3287a95ae37fbcf5e4a3938d0f39d925e799a11f
dr-phil673 <141211581+dr-phil673@users.noreply.github.com>
2023-08-09 00:16:12 +0300

    Update main.py

--- main.py
+++ main.py
@@ -11,7 +11,7 @@ class OCR:
         self.texto = ""
         self.analisar_texto = False
 
-    def inicial(self):
+    def main(self):
         st.title("OCR Programa")
         st.write("Optical Character Recognition (OCR) implementado com Python")
         imagem = st.file_uploader("Selecione alguma imagem", type=["png","jpg"])
@@ -19,48 +19,48 @@ class OCR:
             img = Image.open(imagem)
             st.image(img, width=350)
             st.info("Texto extraído")
-            self.texto = self.extrair_texto(img)
+            self.texto = self.extract(img)
             st.write("{}".format(self.texto))
             
             self.analisar_texto = st.sidebar.checkbox("Analisar texto")
             if self.analisar_texto==True:
-                self.mostrar_analise()
+                cpf = fc.buscar_cpf(self.texto)
+                datas = fc.buscar_data(self.texto)
+                p_boas, percentual_bom = fc.buscar_palavras_boas(self.texto)
+                p_mas, percentual_mau = fc.buscar_palavras_mas(self.texto)
+                
+                if cpf==None:
+                    st.warning("Nenhum CPF encontrado.")
+                else:
+                    cpf = fc.sumarizar_cpf(cpf)
+                    st.success("CPF encontrado:")
+                    st.write(cpf)
+        
+                if datas==None:
+                    st.warning("Nenhuma data encontrada.")
+                else:
+                    datas = fc.sumarizar_datas(datas)
+                    st.success("Datas encontradas:")
+                    st.write(datas)
+                
+                if p_boas==0:
+                    st.warning("Não identificado palavras de bem.")
+                else:
+                    st.success("Palavras de bem:")
+                    st.write("{} palavra(s). Representam das palavras do texto: {:.2f}%".format(p_boas, percentual_bom))
+                
+                if p_mas==0:
+                    st.warning("Não identificado palavras más.")
+                else:
+                    st.success("Palavras más:")
+                    st.write("{} palavra(s). Representam das palavras do texto: {:.2f}%".format(p_mas, percentual_mau))
+
     
-    def extrair_texto(self, img):
+    def extract(self, img):
         texto = pytesseract.image_to_string(img, lang="por")
         return texto
     
     def mostrar_analise(self):
-        cpf = fc.buscar_cpf(self.texto)
-        datas = fc.buscar_data(self.texto)
-        p_boas, percentual_bom = fc.buscar_palavras_boas(self.texto)
-        p_mas, percentual_mau = fc.buscar_palavras_mas(self.texto)
         
-        if cpf==None:
-            st.warning("Nenhum CPF encontrado.")
-        else:
-            cpf = fc.sumarizar_cpf(cpf)
-            st.success("CPF encontrado:")
-            st.write(cpf)
-
-        if datas==None:
-            st.warning("Nenhuma data encontrada.")
-        else:
-            datas = fc.sumarizar_datas(datas)
-            st.success("Datas encontradas:")
-            st.write(datas)
-        
-        if p_boas==0:
-            st.warning("Não identificado palavras de bem.")
-        else:
-            st.success("Palavras de bem:")
-            st.write("{} palavra(s). Representam das palavras do texto: {:.2f}%".format(p_boas, percentual_bom))
-        
-        if p_mas==0:
-            st.warning("Não identificado palavras más.")
-        else:
-            st.success("Palavras más:")
-            st.write("{} palavra(s). Representam das palavras do texto: {:.2f}%".format(p_mas, percentual_mau))
-
 ocr = OCR()
-ocr.inicial()
+ocr.main()
